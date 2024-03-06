88ffe11ae15ea5819ff38a450a003f2dbecf21e7
Joseph Hickey <joseph.p.hickey@nasa.gov>
2024-03-06 13:43:21 -0500

    Fix #58, apply name changes to EDS dispatcher
    Renames symbols to match recent EdsLib change

--- fsw/src/bp_eds_dispatch.c
+++ fsw/src/bp_eds_dispatch.c
@@ -11,7 +11,7 @@
 #include "cfe_msg.h"
 
 /* clang-format off */
-static const BP_Application_Component_Telecommand_DispatchTable_t BP_TC_DISPATCH_TABLE =
+static const EdsDispatchTable_BP_Application_CFE_SB_Telecommand_t BP_TC_DISPATCH_TABLE =
 {
     .CMD =
     {
@@ -55,8 +55,8 @@ void BP_AppPipe(const CFE_SB_Buffer_t *BufPtr)
     CFE_MSG_Size_t    MsgSize;
     CFE_MSG_FcnCode_t MsgFc;
 
-    status = BP_Application_Component_Telecommand_Dispatch(CFE_SB_Telecommand_indication_Command_ID, BufPtr,
-                                                           &BP_TC_DISPATCH_TABLE);
+    status = EdsDispatch_BP_Application_Telecommand(CFE_SB_Telecommand_indication_Command_ID_Telecommand, BufPtr,
+                                                    &BP_TC_DISPATCH_TABLE);
 
     if (status != CFE_SUCCESS)
     {
@@ -65,9 +65,8 @@ void BP_AppPipe(const CFE_SB_Buffer_t *BufPtr)
 
         if (status == CFE_STATUS_UNKNOWN_MSG_ID)
         {
-            CFE_EVS_SendEvent(BP_MID_ERR_EID, CFE_EVS_EventType_ERROR,
-                              "L%d TO: Invalid Msg ID Rcvd 0x%x status=0x%08x", __LINE__,
-                              (unsigned int)CFE_SB_MsgIdToValue(MsgId), (unsigned int)status);
+            CFE_EVS_SendEvent(BP_MID_ERR_EID, CFE_EVS_EventType_ERROR, "L%d TO: Invalid Msg ID Rcvd 0x%x status=0x%08x",
+                              __LINE__, (unsigned int)CFE_SB_MsgIdToValue(MsgId), (unsigned int)status);
         }
         else if (status == CFE_STATUS_WRONG_MSG_LENGTH)
         {
