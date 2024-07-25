3fa5cd9be48870a7351d88e745ed775765c61667
Sara Garcia-Beech <sara.garcia-beech@nasa.gov>
2024-07-25 19:02:28 +0000

    DTNN-480 Added initial Time Proxy files

---
+++ fsw/fwp/fwp_timep.c
@@ -0,0 +1,84 @@
+/************************************************************************
+ * NASA Docket No. GSC-18,719-1, and identified as “core Flight System: Bootes”
+ *
+ * Copyright (c) 2020 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ************************************************************************/
+
+/*
+** \file
+**   This file contains the source code for the FWP Time Proxy.
+*/
+
+/*
+** Include
+*/
+
+#include "cfe.h"
+#include "fwp_timep.h"
+
+
+/*
+** Function Definitions
+*/
+
+/* Returns current monotonic time */
+int64_t BPA_TIMEP_GetMonotonicTime(void)
+{
+    OS_time_t TimeMsec;
+
+    CFE_PSP_GetTime(&TimeMsec);
+
+    return (int64_t) OS_TimeGetTotalMilliseconds(TimeMsec);
+}
+
+/* Returns host time epoch */
+void BPA_TIMEP_GetHostEpoch(BPA_TIMEP_Epoch_t *Epoch)
+{
+    Epoch.Year   = CFE_MISSION_TIME_EPOCH_YEAR;
+    Epoch.Day    = CFE_MISSION_TIME_EPOCH_DAY;
+    Epoch.Hour   = CFE_MISSION_TIME_EPOCH_HOUR;
+    Epoch.Minute = CFE_MISSION_TIME_EPOCH_MINUTE;
+    Epoch.Second = CFE_MISSION_TIME_EPOCH_SECOND;
+    Epoch.Msec   = CFE_MISSION_TIME_EPOCH_MICROS / 1000;
+
+    return;
+}
+
+/* Returns current host clock state */
+BPA_TIMEP_ClockState_t BPA_TIMEP_GetHostClockState(void)
+{
+    CFE_TIME_ClockState_Enum_t HostClockState = CFE_TIME_GetClockState();
+
+    if (HostClockState == CFE_TIME_ClockState_VALID)
+    {
+        return BPA_TIMEP_CLOCK_VALID;
+    }
+    
+    return BPA_TIMEP_CLOCK_INVALID;
+}
+
+/* Returns current host time */
+int64_t BPA_TIMEP_GetHostTime(void)
+{
+    CFE_TIME_SysTime_t HostTime;
+    int64_t HostTimeMsec = 0;
+
+    HostTime = CFE_TIME_GetTAI();
+
+    HostTimeMsec += (int64_t) HostTime.Seconds * 1000;
+    HostTimeMsec += (int64_t) CFE_TIME_Sub2MicroSecs(HostTime.Subseconds) / 1000;
+
+    return HostTimeMsec;
+}
---
+++ fsw/fwp/fwp_timep.h
@@ -0,0 +1,100 @@
+/************************************************************************
+ * NASA Docket No. GSC-18,719-1, and identified as “core Flight System: Bootes”
+ *
+ * Copyright (c) 2020 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ************************************************************************/
+
+/**
+** @file
+**   FWP's Time Proxy (TIMEP) header definitions
+*/
+#ifndef FWP_TIMEP_H
+#define FWP_TIMEP_H
+
+
+/*
+** Type Definitions
+*/
+
+/* Time epoch */
+struct
+{
+    uint32_t Year;
+    uint32_t Day;
+    uint32_t Hour;
+    uint32_t Minute;
+    uint32_t Second;
+    uint32_t Msec;
+
+} BPA_TIMEP_Epoch_t;
+
+/* Host clock state */
+enum 
+{
+    BPA_TIMEP_CLOCK_INVALID = 0,
+    BPA_TIMEP_CLOCK_VALID   = 1
+
+} BPA_TIMEP_ClockState_t;
+
+
+/*
+** Exported Functions
+*/
+
+/*
+** Name:  BPA_TIMEP_GetMonotonicTime
+**
+** Purpose:
+**     This function returns the current monotonic time in milliseconds
+**
+** Returns:
+**      int64_t MonotonicTime: Current monotonic time in milliseconds
+*/
+int64_t BPA_TIMEP_GetMonotonicTime(void);
+
+/*
+** Name:  BPA_TIMEP_GetHostEpoch
+**
+** Purpose:
+**     This function populates the provided pointer with the host epoch
+**
+** Parameters:
+**      BPA_TIMEP_Epoch_t *Epoch: Host epoch
+*/
+void BPA_TIMEP_GetHostEpoch(BPA_TIMEP_Epoch_t *Epoch);
+
+/*
+** Name:  BPA_TIMEP_GetHostClockState
+**
+** Purpose:
+**     This function returns the current host clock state
+**
+** Returns:
+**      BPA_TIMEP_ClockState_t State: Host clock state (valid or invalid)
+*/
+BPA_TIMEP_ClockState_t BPA_TIMEP_GetHostClockState(void);
+
+/*
+** Name:  BPA_TIMEP_GetHostTime
+**
+** Purpose:
+**     This function returns the current host time in  milliseconds
+**
+** Returns:
+**      int64_t Time: Current host time in milliseconds
+*/
+int64_t BPA_TIMEP_GetHostTime(void);
+
+#endif /* FWP_TIMEP_H */
