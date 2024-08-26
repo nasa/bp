ca0a785b3b67788a92029206b3c73019b4efb13e
Cameron Sykes <cameron.j.sykes@nasa.gov>
2024-08-26 16:48:47 +0000

    DTNN-332: Finished EVS_Register() proxy and started EVS_SendEvent() proxy

--- fsw/fwp/evp/fwp_evp.c
+++ fsw/fwp/evp/fwp_evp.c
@@ -1,104 +1,42 @@
 /*
- * TODO: Fill in file header, if necessary.
+ * NASA Docket No. GSC-18,587-1 and identified as “The Bundle Protocol Core Flight
+ * System Application (BP) v6.5”
+ *
+ * Copyright © 2020 United States Government as represented by the Administrator of
+ * the National Aeronautics and Space Administration. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 
-/************************************************
- * Includes
- ************************************************/
-
-#include <assert.h>
+/* ======== */
+/* Includes */
+/* ======== */
 #include "cfe.h"
-#include "bp_eventids.h"
-#include "bpl_evm_api.h"
-#include "bpnode_evp_cfs.h"
+#include "fwp_evp.h"
 
-/************************************************
- * Local Data
- ************************************************/
+/* ==================== */
+/* Function definitions */
+/* ==================== */
 
-/*
-** Limits:
-** - Must contain less than or equal to CFE_PLATFORM_EVS_MAX_EVENT_FILTERS entries
-*/
-static const CFE_EVS_BinFilter_t BPNODE_EVP_CFS_EVENT_FILTER[] = {
-    {BP_IO_SEND_ERR_EID, CFE_EVS_FIRST_8_STOP},
-    {BP_IO_RECEIVE_ERR_EID, CFE_EVS_FIRST_8_STOP},
-    {BP_LIB_PROC_ERR_EID, CFE_EVS_FIRST_8_STOP},
-    {BP_LIB_LOAD_ERR_EID, CFE_EVS_FIRST_8_STOP},
-    {BP_LIB_STORE_ERR_EID, CFE_EVS_FIRST_8_STOP},
-    {BP_LIB_ACCEPT_ERR_EID, CFE_EVS_FIRST_8_STOP},
-    {BP_FILE_ERR_EID, CFE_EVS_FIRST_8_STOP},
-};
-
-const size_t BPNODE_EVP_CFS_NUM_EVENT_FILTERS = sizeof(BPNODE_EVP_CFS_EVENT_FILTER) /
-                                                sizeof(BPNODE_EVP_CFS_EVENT_FILTER[0]);
-
-/************************************************
- * Exported Functions
- ************************************************/
-
-/*-----------------------------------------------
- * BPNODE_EVP_Initialize_Impl
- *-----------------------------------------------*/
-BPL_Status_t BPNODE_EVP_Initialize_Impl(void)
+/* CFE_EVS_Register() proxy */
+int64_t AA_FWP_EVP_Register(const void* Filters, uint16_t NumEventFilters, uint16_t FilterScheme)
 {
-    BPL_Status_t ReturnStatus;
-    CFE_Status_t CfeEvsRegisterStatus;
-
-    CfeEvsRegisterStatus = CFE_EVS_Register(BPNODE_EVP_CFS_EVENT_FILTER,
-                                            BPNODE_EVP_CFS_NUM_EVENT_FILTERS,
-                                            CFE_EVS_EventFilter_BINARY);
-
-    if (CfeEvsRegisterStatus != CFE_SUCCESS)
-    {
-        OS_printf("BPNODE_EVP_Initialize_Impl call to CFE_EVS_Register returned error: %u!\n",
-            CfeEvsRegisterStatus);
-        ReturnStatus.ReturnValue = BPL_STATUS_ERROR_PROXY_INIT;
-    }
-    else
-    {
-        ReturnStatus.ReturnValue = BPL_STATUS_SUCCESS;
-    }
-
-    return ReturnStatus;
+    return (int64_t) CFE_EVS_Register(Filters, NumEventFilters, FilterScheme);
 }
 
-/*-----------------------------------------------
- * BPNODE_EVP_TranslateTypeToHost
- *-----------------------------------------------*/
-uint16_t BPNODE_EVP_TranslateTypeToHost(BPL_EM_EventType_t EventType)
-{
-    uint16_t HostEventType;
-    switch (EventType)
-    {
-        case BPL_EM_EventType_DEBUG:
-            HostEventType = CFE_EVS_EventType_DEBUG;
-            break;
-        case BPL_EM_EventType_INFO:
-            HostEventType = CFE_EVS_EventType_INFORMATION;
-            break;
-        case BPL_EM_EventType_WARNING:
-            HostEventType = CFE_EVS_EventType_ERROR;
-            break;
-        case BPL_EM_EventType_ERROR:
-            HostEventType = CFE_EVS_EventType_ERROR;
-            break;
-        case BPL_EM_EventType_CRITICAL:
-            HostEventType = CFE_EVS_EventType_CRITICAL;
-            break;
-        default:
-            /* This default case also captures the BPL_EM_EventType_UNKNOWN case. */
-            HostEventType = CFE_EVS_EventType_ERROR;
-            break;
-    }
-    return HostEventType;
-}
-
-/*-----------------------------------------------
- * BPNODE_EVP_SendEvent_Impl
- *-----------------------------------------------*/
-BPL_Status_t BPNODE_EVP_SendEvent_Impl(uint16_t EventID, BPL_EM_EventType_t EventType,
-                                       char const * EventText, va_list EventTextArgPtr)
+/* CFE_EVS_SendEvent() proxy */
+int64_t AA_FWP_EVP_SendEvent(uint16_t EventID, BPL_EM_EventType_t EventType,
+                             char const* EventText, va_list EventTextArgPtr)
 {
     BPL_Status_t ReturnStatus;
     CFE_Status_t ProxyStatus;
