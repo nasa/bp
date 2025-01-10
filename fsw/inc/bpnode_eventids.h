834e1343cd4890927f52e7fd23a129bb40c0a884
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-01-10 17:57:11 +0000

    DTNN-1032: Fix event ID for NC/AS initialization that got overwritten

--- fsw/inc/bpnode_eventids.h
+++ fsw/inc/bpnode_eventids.h
@@ -121,7 +121,7 @@
 #define BPNODE_GEN_WRKR_UNK_EXIT_CRIT_EID       91
 #define BPNODE_GEN_WRKR_SEM_TK_ERR_EID          92
 #define BPNODE_WKP_SEM_ERR_EID                  93
-#define BPNODE_AS_RESET_ALL_INIT_ERR_EID        94
+#define BPNODE_NC_AS_INIT_ERR_EID               94
 #define BPNODE_DP_SEND_NODE_CNTRS_ERR_EID       95
 #define BPNODE_ADU_REM_CHAN_ERR_EID             96
 #define BPNODE_ADU_REM_STAT_ERR_EID             97
