ec1644ac54d8a99d273699ea5a6be72622a66996
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-03-18 19:37:34 +0000

    DTNN-613: Add tests for CLA proxy

--- unit-test/fwp_clap_tests.c
+++ unit-test/fwp_clap_tests.c
@@ -36,15 +36,60 @@
 
 void Test_BPA_CLAP_ContactSetup_Nominal(void)
 {
+    BPLib_Status_t Status;
+
+    /* Force called functions to return success */
+    UT_SetDefaultReturnValue(UT_KEY(BPNode_ClaIn_Setup), BPLIB_SUCCESS);
+    UT_SetDefaultReturnValue(UT_KEY(BPNode_ClaOut_Setup), BPLIB_SUCCESS);
+
+    /* Run the function under test */
+    Status = BPA_CLAP_ContactSetup(0, "0.0.0.0", BPLIB_MAX_NUM_CONTACTS - 1);
+
+    /* Verify that the function ran successfully */
+    UtAssert_EQ(BPLib_Status_t, Status, BPLIB_SUCCESS);
+
+    /* Verify that the call functions were called the correct amount */
+    UtAssert_STUB_COUNT(BPNode_ClaIn_Setup, 1);
+    UtAssert_STUB_COUNT(BPNode_ClaOut_Setup, 1);
 }
 
 void Test_BPA_CLAP_ContactSetup_ClaInError(void)
 {
+    BPLib_Status_t Status;
+
+    /* Force called functions to cause BPA_CLAP_ContactSetup to return a failure */
+    UT_SetDefaultReturnValue(UT_KEY(BPNode_ClaIn_Setup), BPLIB_CLA_IO_ERROR);
+    UT_SetDefaultReturnValue(UT_KEY(BPNode_ClaOut_Setup), BPLIB_SUCCESS);
+
+    /* Run the function under test */
+    Status = BPA_CLAP_ContactSetup(0, "0.0.0.0", BPLIB_MAX_NUM_CONTACTS - 1);
+
+    /* Verify that the function ran successfully */
+    UtAssert_EQ(BPLib_Status_t, Status, BPLIB_CLA_IO_ERROR);
+
+    /* Verify that the call functions were called the correct amount */
+    UtAssert_STUB_COUNT(BPNode_ClaIn_Setup, 1);
+    UtAssert_STUB_COUNT(BPNode_ClaOut_Setup, 0);
 }
 
 /* Test that the ADU table validation fails when a SendToMsgId is not a valid message ID */
 void Test_BPA_CLAP_ContactSetup_ClaOutError(void)
 {
+    BPLib_Status_t Status;
+
+    /* Force called functions to cause BPA_CLAP_ContactSetup to return a failure */
+    UT_SetDefaultReturnValue(UT_KEY(BPNode_ClaIn_Setup), BPLIB_SUCCESS);
+    UT_SetDefaultReturnValue(UT_KEY(BPNode_ClaOut_Setup), BPLIB_CLA_INIT_SEM_ERROR);
+
+    /* Run the function under test */
+    Status = BPA_CLAP_ContactSetup(0, "0.0.0.0", BPLIB_MAX_NUM_CONTACTS - 1);
+
+    /* Verify that the function ran successfully */
+    UtAssert_EQ(BPLib_Status_t, Status, BPLIB_CLA_INIT_SEM_ERROR);
+
+    /* Verify that the call functions were called the correct amount */
+    UtAssert_STUB_COUNT(BPNode_ClaIn_Setup, 1);
+    UtAssert_STUB_COUNT(BPNode_ClaOut_Setup, 1);
 }
 
 /* Register the test cases to execute with the unit test tool */
