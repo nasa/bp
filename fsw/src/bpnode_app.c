cdb0f9f241d3fac1238894503f9ef2e51de5a2cb
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-04-04 15:15:07 +0000

    DTNN-613: Parent task should not report think a child task

--- fsw/src/bpnode_app.c
+++ fsw/src/bpnode_app.c
@@ -149,7 +149,7 @@ CFE_Status_t BPNode_WakeupProcess(void)
         {
             BPLib_EM_SendEvent(BPNODE_WKP_SEM_ERR_EID,
                                 BPLib_EM_EventType_ERROR,
-                                "[CLA In #%d] Could not wake up task, RC = %d",
+                                "Could not wake up CLA In task #%d, RC = %d",
                                 ContactNum,
                                 OsStatus);
         }
@@ -159,7 +159,7 @@ CFE_Status_t BPNode_WakeupProcess(void)
         {
             BPLib_EM_SendEvent(BPNODE_WKP_SEM_ERR_EID,
                                 BPLib_EM_EventType_ERROR,
-                                "[CLA Out #%d] Could not wake up task, RC = %d",
+                                "Could not wake up CLA Out task #%d, RC = %d",
                                 ContactNum,
                                 OsStatus);
         }
