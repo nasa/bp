7ed3042fc2544e47e49a566a4a6cf5452366a27a
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-04-25 19:00:58 +0000

    DTNN-1272: Create macros for SB CLA In task specifics

--- config/default_bpnode_platform_cfg.h
+++ config/default_bpnode_platform_cfg.h
@@ -82,6 +82,11 @@
  */
 #define BPNODE_GEN_WRKR_STACK_SIZE (8192u)
 
+/**
+ * \brief SB CLA In Task stack size
+ */
+#define BPNODE_SB_CLA_IN_STACK_SIZE  (32768u)
+
 /** @} */
 
 
@@ -167,6 +172,11 @@
  *          The Contact ID is added to the base value to get the final task priority
  */
 #define BPNODE_CLA_IN_PRIORITY_BASE  (BPNODE_ADU_IN_PRIORITY_BASE + BPLIB_MAX_NUM_CHANNELS)
+
+/**
+ * \brief SB CLA In Task priority base
+ */
+#define BPNODE_SB_CLA_IN_PRIORITY_BASE (BPNODE_CLA_IN_PRIORITY_BASE + 1)
 
 /** @} */
 
