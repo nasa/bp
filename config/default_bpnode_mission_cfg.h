befa76da3b1330269cc8062894438df6c2c9867f
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-07-09 20:56:27 +0000

    DTNN-1382: Remove SB CLA contact from contacts table

--- config/default_bpnode_mission_cfg.h
+++ config/default_bpnode_mission_cfg.h
@@ -67,11 +67,11 @@
 /**
  * \brief Default EID Node Number to egress bundles to contact 1
  */
-#define BPNODE_EID_NODE_NUM_FOR_CONTACT_1 400
+// #define BPNODE_EID_NODE_NUM_FOR_CONTACT_1 400
 
 /**
  * \brief Default EID Service Number to egress bundles to contact 1
  */
-#define BPNODE_EID_SERVICE_NUM_FOR_CONTACT_1 42
+// #define BPNODE_EID_SERVICE_NUM_FOR_CONTACT_1 42
 
 #endif /* BPNODE_MISSION_CFG_H */
