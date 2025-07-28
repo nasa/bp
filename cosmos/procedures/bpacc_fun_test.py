71a1055797754b7e7277f6c14973b5b8b7b8b4d3
Cameron Sykes <cameron.j.sykes@nasa.gov>
2025-07-28 21:16:38 +0000

    DTNN-1393: Remove files that are not included in any other procedures

--- cosmos/procedures/bpacc_fun_test.py
+++
@@ -1,8 +0,0 @@
-wait_check_packet(f"<%= target_name %>", "BPACC_FUN_HK", 1, 6)
-cmd_count = tlm(f"<%= target_name %> BPACC_FUN_HK COMMAND_COUNTER")
-
-cmd(f"<%= target_name %> BPACC_FUN_CMD_NOOP")
-wait_check(f"<%= target_name %> BPACC_FUN_HK COMMAND_COUNTER == {cmd_count+1}", 10)
-# Check accepted Reset Counters command
-cmd(f"<%= target_name %> BPACC_FUN_CMD_RESET_COUNTERS")
-wait_check(f"<%= target_name %> BPACC_FUN_HK COMMAND_COUNTER == 0", 10)
--- cosmos/procedures/procedure.rb
+++
@@ -1,3 +0,0 @@
-# Script Runner test script
-cmd("DTNFSW EXAMPLE")
-wait_check("DTNFSW STATUS BOOL == 'FALSE'", 5)
